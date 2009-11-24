b3bfd8395438d04b3ba02dacb5901b5fd4a58e96
Anthony Dahanne <anthony.dahanne@gmail.com>
2009-11-24 23:16:30 +0000

    choosephotonumber in progress...

--- g2android/src/net/dahanne/android/g2android/activity/ChoosePhotoNumber.java
+++ g2android/src/net/dahanne/android/g2android/activity/ChoosePhotoNumber.java
@@ -16,27 +16,61 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package net.dahanne.android.g2android.activity;
+
 import net.dahanne.android.g2android.R;
 import net.dahanne.android.g2android.utils.modified_android_source.NumberPicker;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 
-public class ChoosePhotoNumber extends Activity {
-	
+public class ChoosePhotoNumber extends Activity implements OnClickListener {
+
 	private NumberPicker np;
-	
+	private Button okButton;
+	private Button cancelButton;
+	private Button firstPhotoButton;
+	private Button lastPhotoButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.choose_photo_number);
-		setTitle(R.string.choose_photo_number_title);
+		np = (NumberPicker) findViewById(R.id.photo_number);
+		okButton = (Button) findViewById(R.id.ok);
+		cancelButton = (Button) findViewById(R.id.cancel);
+		firstPhotoButton = (Button) findViewById(R.id.first_photo);
+		lastPhotoButton = (Button) findViewById(R.id.last_photo);
 
+		setTitle(R.string.choose_photo_number_title);
+		np.setRange(0, 100, null);
+		okButton.setOnClickListener(this);
+		cancelButton.setOnClickListener(this);
 
 	}
 
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.ok:
+			int currentNumber = np.getCurrent();
+			Log.d("yop", Integer.toString(currentNumber));
+			// TODO
+			break;
+		case R.id.cancel:
+			this.finish();
+			break;
+		case R.id.first_photo:
+			// TODO
+			break;
+		case R.id.last_photo:
+			// TODO
+			break;
+		default:
+			break;
+		}
+
+	}
 }
