fb7b55c9c479ec76233c5ad9e76799d1b77d2f99
anthony.dahanne <anthony.dahanne@3dfd1a40-884d-11de-9c77-2f49170b69f2>
2010-03-03 03:49:02 +0000

    1.4.3; bug fixes : #25 : embedded galleries now available; #27 error when quickly scrolling solved; #30 seeing g2 temp pictures while using the gallery : solved, thanks to .nomedia

---
+++ 1.4.3/g2android/src/net/dahanne/android/g2android/utils/ShowUtils.java
@@ -0,0 +1,115 @@
+/**
+ * g2android
+ * Copyright (c) 2009 Anthony Dahanne
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package net.dahanne.android.g2android.utils;
+
+import net.dahanne.android.g2android.G2AndroidApplication;
+import net.dahanne.android.g2android.R;
+import net.dahanne.android.g2android.utils.DBHelper.G2AndroidContext;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.Toast;
+
+/**
+ * @author Anthony Dahanne
+ * 
+ */
+public class ShowUtils {
+
+	private static ShowUtils showUtils = new ShowUtils();
+
+	public static ShowUtils getInstance() {
+		return showUtils;
+	}
+
+	private ShowUtils() {
+
+	}
+
+	public void alertConnectionProblem(String exceptionMessage,
+			String galleryUrl, Context context) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		// if there was an exception thrown, show it, or tell to verify
+		// settings
+		String message = context.getString(R.string.not_connected) + galleryUrl
+				+ context.getString(R.string.exception_thrown)
+				+ exceptionMessage;
+		builder.setTitle(R.string.problem).setMessage(message)
+				.setPositiveButton(R.string.ok,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								dialog.cancel();
+							}
+						});
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+
+	public void toastAlbumSuccessfullyCreated(Context context) {
+		Toast.makeText(context,
+				context.getString(R.string.album_successfully_created),
+				Toast.LENGTH_LONG).show();
+	}
+
+	public void toastImageSuccessfullyAdded(Context context) {
+		Toast.makeText(context,
+				context.getString(R.string.image_successfully_created),
+				Toast.LENGTH_LONG).show();
+	}
+	
+	public void toastCacheSuccessfullyCleared(Context context) {
+		Toast.makeText(context,
+				context.getString(R.string.cache_cleared),
+				Toast.LENGTH_LONG).show();
+	}
+
+	/**
+	 * 
+	 * @param activity
+	 * @return true if we need to login
+	 */
+	public boolean recoverContextFromDatabase(Activity activity) {
+		DBHelper dbHelper = new DBHelper(activity);
+		G2AndroidContext g2c = dbHelper.getLast();
+		if (g2c != null) {
+			((G2AndroidApplication) activity.getApplication())
+					.setCurrentPosition(g2c.currentPosition);
+			((G2AndroidApplication) activity.getApplication())
+					.setRootAlbum(g2c.rootAlbum);
+			((G2AndroidApplication) activity.getApplication())
+					.setAlbumName(g2c.albumName);
+			if (g2c.isLoggedIn == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void saveContextToDatabase(Activity activity) {
+		DBHelper dbHelper = new DBHelper(activity);
+		dbHelper.insert(new G2AndroidContext(0,
+				((G2AndroidApplication) activity.getApplication())
+						.getCurrentPosition(), ((G2AndroidApplication) activity
+						.getApplication()).getRootAlbum(),
+				((G2AndroidApplication) activity.getApplication())
+						.getAlbumName(), G2ConnectionUtils.getInstance()
+						.getSessionCookies().size() > 1));
+	}
+
+}
---
+++ 1.4.3/g2android/test/net/dahanne/android/g2android/utils/G2ConnectionUtilsTest.java
@@ -0,0 +1,197 @@
+/*
+ * G2Android
+ * Copyright (c) 2009 Anthony Dahanne
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package net.dahanne.android.g2android.utils;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Map.Entry;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * These tests should run, provided that the gallery at http://g2.dahanne.net is
+ * up and running !
+ * 
+ * @author Anthony Dahanne
+ * 
+ */
+public class G2ConnectionUtilsTest extends Assert {
+	private String galleryUrl;
+	private String user;
+	private String password;
+	private G2ConnectionUtils g2ConnectionUtils;
+
+	@Before
+	public void init() {
+		galleryUrl = "http://g2.dahanne.net/";
+		user = "g2android";
+		password = "g2android";
+		g2ConnectionUtils = G2ConnectionUtils.getInstance();
+	}
+
+	@Test
+	public void fetchImagesTest() throws GalleryConnectionException {
+		HashMap<String, String> fetchImages = g2ConnectionUtils.fetchImages(
+				galleryUrl, 11);
+		assertTrue(fetchImages.size() != 0);
+		assertTrue(fetchImages.containsKey("image_count"));
+		assertFalse(fetchImages.get("image_count").equals("0"));
+
+		fetchImages = g2ConnectionUtils.fetchImages(galleryUrl, 9999);
+		// just debug messages
+		assertTrue(fetchImages.size() == 3);
+
+	}
+
+	@Test(expected = GalleryConnectionException.class)
+	public void fetchImagesTest__exception() throws GalleryConnectionException {
+		g2ConnectionUtils.fetchImages("http://something.thatmaynotexist.com",
+				11);
+
+	}
+
+	@Test
+	public void checkGalleryUrlIsValidTest() throws GalleryConnectionException {
+
+		// I know this is not a galleryUrl...
+		String falseGalleryHost = "www.google.com";
+		assertFalse(g2ConnectionUtils.checkGalleryUrlIsValid(falseGalleryHost));
+
+		// This is supposed to be a valid Url !
+		assertTrue(g2ConnectionUtils.checkGalleryUrlIsValid(galleryUrl));
+
+	}
+
+	@Test
+	public void loginToGalleryTest() throws GalleryConnectionException {
+
+		String authToken = g2ConnectionUtils.loginToGallery(galleryUrl,
+				"hacker", "hackerPassword");
+		assertNull(authToken);
+
+		authToken = g2ConnectionUtils
+				.loginToGallery(galleryUrl, user, password);
+		assertNotNull(authToken);
+		// if we're logged in, we should see the g2android album
+		HashMap<String, String> fetchAlbums = g2ConnectionUtils
+				.fetchAlbums(galleryUrl);
+		boolean found = false;
+		for (Entry<String, String> entry : fetchAlbums.entrySet()) {
+			if (entry.getValue().equals("G2AndroidSecretAlbum")) {
+				found = true;
+			}
+		}
+		assertEquals(true, found);
+
+	}
+
+	/**
+	 * This test fail because the user can't add a photo item in given album
+	 * 
+	 * @throws GalleryConnectionException
+	 */
+	@Test(expected = GalleryConnectionException.class)
+	public void sendImageToGalleryTestFail() throws GalleryConnectionException {
+		g2ConnectionUtils.loginToGallery(galleryUrl, user, password);
+		File imageFile = new File("image.png");
+		g2ConnectionUtils.sendImageToGallery(galleryUrl, -5, imageFile);
+	}
+
+	@Test
+	public void sendImageToGalleryTest() throws GalleryConnectionException {
+		g2ConnectionUtils.loginToGallery(galleryUrl, user, password);
+		File imageFile = new File("image.png");
+		int newItemId = g2ConnectionUtils.sendImageToGallery(galleryUrl, 174,
+				imageFile);
+		assertTrue(newItemId != 0);
+	}
+
+	@Test
+	public void createNewAlbumTest() throws GalleryConnectionException {
+		g2ConnectionUtils.loginToGallery(galleryUrl, user, password);
+		Random random = new Random();
+		int randomInt = random.nextInt();
+		String albumName = "UnitTestAlbumNumber" + randomInt;
+		String albumTitle = "Unit Test Album";
+		String albumDescription = "Yet another Unit Test Album !";
+		int newAlbumName = g2ConnectionUtils.createNewAlbum(galleryUrl, 174,
+				albumName, albumTitle, albumDescription);
+		assertTrue(newAlbumName != 0);
+	}
+
+	@Test
+	public void getPortFromUrlTest() {
+		int portFromUrl = g2ConnectionUtils
+				.getPortFromUrl(galleryUrl + ":4343");
+		assertEquals(4343, portFromUrl);
+
+		portFromUrl = g2ConnectionUtils.getPortFromUrl(galleryUrl
+				+ ":4343/gallery2");
+		assertEquals(4343, portFromUrl);
+
+		portFromUrl = g2ConnectionUtils.getPortFromUrl("https://url.net");
+		assertEquals(443, portFromUrl);
+
+		portFromUrl = g2ConnectionUtils.getPortFromUrl("https://url.net:4343");
+		assertEquals(4343, portFromUrl);
+
+		portFromUrl = g2ConnectionUtils
+				.getPortFromUrl("https://url.net:4343/gallery2");
+		assertEquals(4343, portFromUrl);
+
+	}
+
+	@Test
+	public void getPathFromUrlTest() {
+		String pathFromUrl = g2ConnectionUtils
+				.getPathFromUrl("http://g2.dahanne.net/gallery2");
+		assertEquals("/gallery2", pathFromUrl);
+
+		pathFromUrl = g2ConnectionUtils
+				.getPathFromUrl("http://g2.dahanne.net:8080/gallery2");
+		assertEquals("/gallery2", pathFromUrl);
+		pathFromUrl = g2ConnectionUtils
+				.getPathFromUrl("https://g2.dahanne.net:8080/gallery2");
+		assertEquals("/gallery2", pathFromUrl);
+		pathFromUrl = g2ConnectionUtils.getPathFromUrl("https://dahanne.net");
+		assertEquals("/", pathFromUrl);
+
+	}
+
+	@Test
+	public void getHostFromUrlTest() {
+		String pathFromUrl = g2ConnectionUtils
+				.getHostFromUrl("http://g2.dahanne.net/gallery2");
+		assertEquals("g2.dahanne.net", pathFromUrl);
+
+		pathFromUrl = g2ConnectionUtils
+				.getHostFromUrl("http://g2.dahanne.net:8080/gallery2");
+		assertEquals("g2.dahanne.net", pathFromUrl);
+		pathFromUrl = g2ConnectionUtils
+				.getHostFromUrl("https://g2.dahanne.net:8080/gallery2");
+		assertEquals("g2.dahanne.net", pathFromUrl);
+		pathFromUrl = g2ConnectionUtils.getHostFromUrl("https://dahanne.net");
+		assertEquals("dahanne.net", pathFromUrl);
+
+	}
+
+}
