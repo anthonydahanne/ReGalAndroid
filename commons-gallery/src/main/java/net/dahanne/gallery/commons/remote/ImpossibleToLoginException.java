ad898ec7b14ff62b634e7203d0cd874545997c99
Anthony Dahanne <anthony.dahanne@gmail.com>
2010-10-28 23:03:50 -0400

    adding g2-java-client and commons-gallery

---
+++ commons-gallery/src/main/java/net/dahanne/gallery/commons/remote/ImpossibleToLoginException.java
@@ -0,0 +1,15 @@
+package net.dahanne.gallery.commons.remote;
+
+public class ImpossibleToLoginException extends GalleryConnectionException {
+
+	private static final long serialVersionUID = -8277215227949719569L;
+
+	public ImpossibleToLoginException(String msg) {
+		super(msg);
+	}
+
+	public ImpossibleToLoginException(Throwable t) {
+		super(t);
+	}
+
+}
