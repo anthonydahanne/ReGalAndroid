19bc9e72521e5eba24348da9fb5e43993d8d68a5
Anthony Dahanne <anthony.dahanne@gmail.com>
2012-02-27 01:06:27 -0500

    travis-ci : creating and starting an android emulator

--- bin/ci/get-android-sdk.sh
+++ bin/ci/get-android-sdk.sh
@@ -11,6 +11,8 @@ android-sdk-linux/tools/android update sdk --no-ui -t platform-tools
 echo "here is the list of the downloaded platforms : "
 ls android-sdk-linux/platforms
 echo "creating an emulator"
-android-sdk-linux/tools/android create avd -n android2.3 -t 9 -c 16M -b armeabi
+#the echo no | before the command is to answer no to the question "do you wish to create a custom hardware profile"
+#see http://stackoverflow.com/questions/7931997/setting-android-avd-command-line-hardware-profile
+echo no | android-sdk-linux/tools/android create avd -n android2.3.3 -t android-10 -c 16M -b armeabi
 echo "starting the emulator in background with no window"
 android-sdk-linux/tools/emulator-arm -avd android2.3 -no-window
